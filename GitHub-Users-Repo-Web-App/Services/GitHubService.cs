97132d82557508773f8db2d1121abb5a7bf69455
Priya Bhatti <69536106+stvrbuckz@users.noreply.github.com>
2025-05-14 15:59:12 +0100

    added summary comments for methods in GitHubService

--- GitHub-Users-Repo-Web-App/Services/GitHubService.cs
+++ GitHub-Users-Repo-Web-App/Services/GitHubService.cs
@@ -19,6 +19,9 @@ namespace GitHub_Users_Repo_Web_App.Services
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// This method returns the HTTP Status Code of the GitHub API.
+        /// </summary>
         public async Task<HttpStatusCode> GetGitHubApiStatus()
         {
             var url = "https://api.github.com/users";
@@ -26,6 +29,11 @@ namespace GitHub_Users_Repo_Web_App.Services
             return response.StatusCode;
         }
 
+
+        /// <summary>
+        /// This method returns true or false if the user exists on GitHub via the API.
+        /// </summary>
+        /// <param name="username">the username of the user to search for.</param>
         public async Task<bool> CheckUserGitHubExists(string username)
         {
             var url = $"https://api.github.com/users/{username}";
@@ -39,6 +47,10 @@ namespace GitHub_Users_Repo_Web_App.Services
             return false;
         }
 
+        /// <summary>
+        /// This method returns the GitHub user details and their top 5 stargazer repositories.
+        /// </summary>
+        /// <param name="username">the username of the user to search for.</param>
         public async Task<GitHubUserDetails> GetGitHubUserDetails(string username)
         {
             var response = await GetGitHubUserDetailsResponse(username);
@@ -57,6 +69,10 @@ namespace GitHub_Users_Repo_Web_App.Services
             } 
         }
 
+        /// <summary>
+        /// This method returns the GitHub user details response from the GitHub API.
+        /// </summary>
+        /// <param name="username">the username of the user to search for.</param>
         private async Task<string> GetGitHubUserDetailsResponse(string username)
         {
             var url = $"https://api.github.com/users/{username}";
@@ -68,6 +84,10 @@ namespace GitHub_Users_Repo_Web_App.Services
             return responseContent;
         }
 
+        /// <summary>
+        /// This method returns the GitHub repositories of the user from the GitHub API.
+        /// </summary>
+        /// <param name="username">the username of the user to search for.</param>
         private async Task<List<GitHubRepo>> GetGitHubRepos(string username)
         {
             var url = $"https://api.github.com/users/{username}/repos";
@@ -77,6 +97,10 @@ namespace GitHub_Users_Repo_Web_App.Services
             return _mapper.MapRepos(responseContent);
         }
 
+        /// <summary>
+        /// This method returns the top five stargazer repositories of the user with the highest stargazer count.
+        /// </summary>
+        /// <param name="gitHubRepos">the list of GitHub repositories.</param>
         private List<GitHubRepo> GetTopStargazerRepos(List<GitHubRepo> gitHubRepos)
         {
             var topStargazerRepos = gitHubRepos.OrderByDescending(repo => repo.StargazersCount)
